05059b1d747d95ee6286dbb73125fe1b7cb9629e
Tiago Bassani <tiagolbassani@gmail.com>
2023-05-29 16:46:35 -0300

    Code and test scoop options images from server

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ScoopOptions from "./ScoopOptions";
+import Row from "react-bootstrap/Row";
+
+const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+
+  //optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((err) => {
+        //TODO: handle error response
+      });
+  }, [optionType]);
+
+  //TODO: replace 'null' with ToppingOptions when available
+  const ItemComponent = optionType === "scoops" ? ScoopOptions : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+};
+
+export default Options;
